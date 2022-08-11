e77f8d017acc602bea7ebc803d8ec58847df5676
d1vious <josehelps@gmail.com>
2022-08-11 18:33:04 -0400

    adding splunk detections to the lolbas yml

--- scripts/enrich_with_splunk.py
+++ scripts/enrich_with_splunk.py
@@ -7,7 +7,7 @@ from os import path, walk
 from tqdm import tqdm
 
 
-def  read_security_content_detections(SECURITY_CONTENT_PATH):
+def read_security_content_detections(SECURITY_CONTENT_PATH, VERBOSE):
     types = ["endpoint", "application", "cloud", "network", "web", "experimental", "deprecated"]
     manifest_files = []
     for t in types:
@@ -20,7 +20,7 @@ def  read_security_content_detections(SECURITY_CONTENT_PATH):
     for manifest_file in tqdm(manifest_files):
         detection_yaml = dict()
         if VERBOSE:
-            print("processing yaml {0}".format(manifest_file))
+            print("processing detection yaml {0}".format(manifest_file))
 
         with open(manifest_file, 'r') as stream:
             try:
@@ -35,7 +35,7 @@ def  read_security_content_detections(SECURITY_CONTENT_PATH):
         detections.append(detection_yaml)
     return detections
 
-def read_lolbas(LOLBAS_PATH):
+def read_lolbas(LOLBAS_PATH, VERBOSE):
     types = ["OSBinaries", "OSLibraries", "OSScripts", "OtherMSBinaries"]
     manifest_files = []
     for t in types:
@@ -48,7 +48,7 @@ def read_lolbas(LOLBAS_PATH):
     for manifest_file in tqdm(manifest_files):
         lolba_yaml = dict()
         if VERBOSE:
-            print("processing yaml {0}".format(manifest_file))
+            print("processing lolba yaml {0}".format(manifest_file))
 
         with open(manifest_file, 'r') as stream:
             try:
@@ -64,14 +64,54 @@ def read_lolbas(LOLBAS_PATH):
 
 def confirm_match(lolba, matching_id_detections):
     matching_detections = []
+    # grab just the name but not extension
     search_word = lolba['Name'].split('.')[0]
+    # remove any (64) entries
+    search_word = re.sub(r'\(\d+\)', '', search_word)
+
     for detection in matching_id_detections:
         if re.findall(search_word, detection['name'], re.IGNORECASE):
             matching_detections.append(detection)
 
     return matching_detections
 
-def enrich_lolbas(detections,lolbas):
+def insert_splunk_detections(lolba, matching_detections):
+    splunk_detections = []
+
+    # build splunk detection entry object
+    for matching_detection in matching_detections:
+        detection_entry = {'Splunk' : "https://research.splunk.com/" + matching_detection['kind'] + "/" + matching_detection['id'] + "/"}
+        splunk_detections.append(detection_entry)
+    
+    # clean up current splunk entries
+    lolba_detections = []
+    if 'Detection' in lolba and lolba['Detection'] != None:
+        for detection in lolba['Detection']:
+            lolba_detections.append(detection)
+
+    # extend cleaned up detections with correct splunk urls
+    lolba_detections.extend(splunk_detections)
+
+    # replace list with newly cleaned 
+    lolba['Detection'] = lolba_detections
+
+    return lolba
+
+def unique_detections(lolba_with_detections, lolba, VERBOSE):
+    # unique all detections
+    unique_detection_list = []
+    if 'Detection' in lolba_with_detections and lolba_with_detections['Detection'] != None:
+        for detection in lolba_with_detections['Detection']:
+            if detection in unique_detection_list:
+                pass
+            else:
+                if VERBOSE:
+                    print("enriching lolba {0} with matching splunk detection: {1}".format(lolba['Name'], detection))
+                unique_detection_list.append(detection)
+        lolba['Detection'] = unique_detection_list    
+    return lolba
+    
+def enrich_lolbas(detections, lolbas, VERBOSE):
     detections_with_mitre = dict()
     enriched_lolbas = []
 
@@ -90,38 +130,26 @@ def enrich_lolbas(detections,lolbas):
                 else:
                     detections_with_mitre[mitre_id].append(detection_obj)
     
+    lolba_with_detections = dict()
     for lolba in lolbas:
         for command in lolba['Commands']:
             if 'MitreID' in command:
                 if command['MitreID'] in detections_with_mitre:
                     matching_id_detections = detections_with_mitre[command['MitreID']]
                     matching_detections = confirm_match(lolba,matching_id_detections)
-                    for matching_detection in matching_detections:
-                        print(lolba['Name'], matching_detection['name'])
-                        detection_string = 'Splunk: https://research.splunk.com/' + matching_detection['kind'] + '/' + matching_detection['id'] + '/'
-                        if 'Detection' in lolba and lolba['Detection'] != None:
-                            lolba['Detection'].append(detection_string)
-                        else:
-                            lolba['Detection'] = []
-                            lolba['Detection'].append(detection_string)
-
+                    lolba_with_detections = insert_splunk_detections(lolba, matching_detections)
+                    
         # unique all detections
-        unique_detection_list = []
-        if 'Detection' in lolba and lolba['Detection'] != None:
-            for detection in lolba['Detection']:
-                if detection in unique_detection_list:
-                    pass
-                else:
-                    unique_detection_list.append(detection)
-            lolba['Detection'] = unique_detection_list    
+        lolba = unique_detections(lolba_with_detections, lolba, VERBOSE)
         enriched_lolbas.append(lolba)
     return enriched_lolbas
 
-def write_lolbas(enriched_lolbas, LOLBAS_PATH):
+def write_lolbas(enriched_lolbas, LOLBAS_PATH, VERBOSE):
     for lolba in enriched_lolbas:
         file_path = lolba['file_path']
         lolba.pop('file_path')
-        print(yaml.dump(lolba, indent=2))
+        if VERBOSE:
+            print(yaml.dump(lolba, indent=2))
         with open(file_path, 'w') as outfile:
             yaml.dump(lolba, outfile, default_flow_style=False, sort_keys=False)
 
@@ -145,13 +173,13 @@ if __name__ == "__main__":
         sys.exit(1)
 
     print("processing splunk security content detections")
-    detections = read_security_content_detections(SECURITY_CONTENT_PATH)
+    detections = read_security_content_detections(SECURITY_CONTENT_PATH, VERBOSE)
     print("processing lolbas")
-    lolbas = read_lolbas(LOLBAS_PATH)
+    lolbas = read_lolbas(LOLBAS_PATH, VERBOSE)
     print("enriching lolbas")
-    enriched_lolbas = enrich_lolbas(detections, lolbas)
+    enriched_lolbas = enrich_lolbas(detections, lolbas, VERBOSE)
     print("writing enriched lolbas")
-    write_lolbas(enriched_lolbas, LOLBAS_PATH)
+    write_lolbas(enriched_lolbas, LOLBAS_PATH, VERBOSE)
 
 
 
